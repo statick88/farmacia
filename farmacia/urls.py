7994767d2db38d719c5df45bfd887e0f3192c8c6
Diego Saavedra <dsaavedra88@gmail.com>
2024-12-05 15:50:20 -0500

    add / url medicamentos

--- farmacia/urls.py
+++ farmacia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.lista_medicamentos, name='lista_medicamentos'),
     path('medicamentos/', views.lista_medicamentos, name='lista_medicamentos'),
     path('medicamentos/<int:id>/', views.detalle_medicamento, name='detalle_medicamento'),
     path('medicamentos/nuevo/', views.crear_medicamento, name='crear_medicamento'),
